f0fc9b86d6d10d0d1ee8ba3b08a53d2630925c90
GhadahAlb <ghadah.b13@gmail.com>
2023-10-16 23:55:22 +0300

    Add Arrow button to Najed , Shamaliah and Janob regions + صوت الحمص sound

---
+++ FenjalAlhaif/FenjalAlhaif/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  FenjalAlhaif
+//
+//  Created by Ghadah Albassam on 16/10/2023.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
