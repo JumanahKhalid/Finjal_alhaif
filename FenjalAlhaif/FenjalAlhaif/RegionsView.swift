4db90fa3f020aa1897d6c8acce990604b0b4d344
JumanahKhalid <jumanahkhalid45@gmail.com>
2023-10-16 13:00:04 +0300

    Refactor region view

---
+++ FenjalAlhaif/FenjalAlhaif/RegionsView.swift
@@ -0,0 +1,18 @@
+//
+//  RegionsView.swift
+//  FenjalAlhaif
+//
+//  Created by jumanah khalid albisher on 01/04/1445 AH.
+//
+
+import SwiftUI
+
+struct RegionsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegionsView()
+}
