df3176ae649b0816ce8865615ba307bf61fdd512
JumanahKhalid <jumanahkhalid45@gmail.com>
2023-10-16 13:00:32 +0300

    refactor region view

--- FenjalAlhaif/FenjalAlhaif/ContentView.swift
+++ FenjalAlhaif/FenjalAlhaif/ContentView.swift
@@ -100,7 +100,7 @@ struct ContentView: View {
                         .shadow(radius:50)
                         .offset(CGSize(width: -30, height: 30))
                     
-                    NavigationLink("", destination:  Regions(), isActive: $didTap)
+                    NavigationLink("", destination:  RegionsView(), isActive: $didTap)
                     EmptyView()
                        .onAppear {
                             if !isNextPageActive {
@@ -128,97 +128,7 @@ struct ContentView: View {
 }
 
 
-struct Regions: View {
-  @StateObject private var audioPlayerManager = AudioPlayerManager.shared
-    @State var selectedRegion: String = ""
-    @State private var isNextPageActive = false
-    @State private var isPlaying = false
-    var body: some View {
-        NavigationStack {
-            HStack {
-                ZStack {
-                    GifBackground(gifName: "# 2 Options page")
-                    
-                    Button("المنطقة الوسطى") {
-                        selectedRegion = "Najed"
-                    }
-                    .font(.system(size: 20))
-                    .buttonStyle(.bordered)
-                    .tint(.clear)
-                    .offset(x: 10, y: 55)
-                    
-                    NavigationLink(
-                        destination: NajdRegion(gifName: "oven1"),
-                        isActive: Binding<Bool>(
-                            get: { selectedRegion == "Najed" },
-                            set: { isActive in
-                                if !isActive {
-                                    selectedRegion = ""
-                                }
-                            }
-                        ),
-                        label: { EmptyView() }
-                    )
-                    
-                    Button("المنطقة الشمالية") {
-                        selectedRegion = "Shamaliah"
-                    }
-                    .font(.system(size: 20))
-                    .buttonStyle(.bordered)
-                    .tint(.clear)
-                    .offset(x: 240, y: 55)
-                    
-                    NavigationLink(
-                        destination: ShamaliahRegion(gifName: "oven1"),
-                        isActive: Binding<Bool>(
-                            get: { selectedRegion == "Shamaliah" },
-                            set: { isActive in
-                                if !isActive {
-                                    selectedRegion = ""
-                                }
-                            }
-                        ),
-                        label: { EmptyView() }
-                    )
-                    
-                    Button("المنطقة الجنوبية") {
-                        selectedRegion = "Janob"
-                    }
-                    .font(.system(size: 20))
-                    .buttonStyle(.bordered)
-                    .tint(.clear)
-                    .offset(x: -240, y: 55)
-                    
-                    NavigationLink(
-                        destination: JanobRegion(gifName: "oven1"),
-                        isActive: Binding<Bool>(
-                            get: { selectedRegion == "Janob" },
-                            set: { isActive in
-                                if !isActive {
-                                    selectedRegion = ""
-                                }
-                            }
-                        ),
-                        label: { EmptyView() }
-                    )
-                }
-                .onAppear {
-                     if !isNextPageActive {
-                         audioPlayerManager.playBackgroundMusic()
-                         isPlaying = true
-                     }
-                 }
-                 .onDisappear {
-                     if isPlaying {
-                         audioPlayerManager.stopBackgroundMusic()
-                         isPlaying = false
-                     }
-                 }
-         }
-        }
-                .edgesIgnoringSafeArea(.all)
-            }
-        }
+
     
 
 
--- FenjalAlhaif/FenjalAlhaif/RegionsView.swift
+++ FenjalAlhaif/FenjalAlhaif/RegionsView.swift
@@ -8,11 +8,93 @@
 import SwiftUI
 
 struct RegionsView: View {
+  @StateObject private var audioPlayerManager = AudioPlayerManager.shared
+    @State var selectedRegion: String = ""
+    @State private var isNextPageActive = false
+    @State private var isPlaying = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-#Preview {
-    RegionsView()
-}
+        NavigationStack {
+            HStack {
+                ZStack {
+                    GifBackground(gifName: "# 2 Options page")
+                    
+                    Button("المنطقة الوسطى") {
+                        selectedRegion = "Najed"
+                    }
+                    .font(.system(size: 20))
+                    .buttonStyle(.bordered)
+                    .tint(.clear)
+                    .offset(x: 10, y: 55)
+                    
+                    NavigationLink(
+                        destination: NajdRegion(gifName: "oven1"),
+                        isActive: Binding<Bool>(
+                            get: { selectedRegion == "Najed" },
+                            set: { isActive in
+                                if !isActive {
+                                    selectedRegion = ""
+                                }
+                            }
+                        ),
+                        label: { EmptyView() }
+                    )
+                    
+                    Button("المنطقة الشمالية") {
+                        selectedRegion = "Shamaliah"
+                    }
+                    .font(.system(size: 20))
+                    .buttonStyle(.bordered)
+                    .tint(.clear)
+                    .offset(x: 240, y: 55)
+                    
+                    NavigationLink(
+                        destination: ShamaliahRegion(gifName: "oven1"),
+                        isActive: Binding<Bool>(
+                            get: { selectedRegion == "Shamaliah" },
+                            set: { isActive in
+                                if !isActive {
+                                    selectedRegion = ""
+                                }
+                            }
+                        ),
+                        label: { EmptyView() }
+                    )
+                    
+                    Button("المنطقة الجنوبية") {
+                        selectedRegion = "Janob"
+                    }
+                    .font(.system(size: 20))
+                    .buttonStyle(.bordered)
+                    .tint(.clear)
+                    .offset(x: -240, y: 55)
+                    
+                    NavigationLink(
+                        destination: JanobRegion(gifName: "oven1"),
+                        isActive: Binding<Bool>(
+                            get: { selectedRegion == "Janob" },
+                            set: { isActive in
+                                if !isActive {
+                                    selectedRegion = ""
+                                }
+                            }
+                        ),
+                        label: { EmptyView() }
+                    )
+                }
+                .onAppear {
+                     if !isNextPageActive {
+                         audioPlayerManager.playBackgroundMusic()
+                         isPlaying = true
+                     }
+                 }
+                 .onDisappear {
+                     if isPlaying {
+                         audioPlayerManager.stopBackgroundMusic()
+                         isPlaying = false
+                     }
+                 }
+         }
+        }
+                .edgesIgnoringSafeArea(.all)
+            }
+        }
